b35142719bbe7cc58069528af333e170d9b59c4c
polinajakovleva1 <jakovleva.polina420420@gmail.com>
2026-02-11 18:03:55 +0500

    feat: add config.py with env variables

--- src/app/main.py
+++ src/app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from .core.config import settings
 
 app = FastAPI(
     title="FastAPI Blog Backend",
@@ -8,4 +9,12 @@ app = FastAPI(
 
 @app.get("/")
 async def root():
-    return {"messages": "Работает!!!", "status": "ok"}
+    return {
+        "project_name": settings.PROJECT_NAME,
+        "debug": settings.DEBUG,
+        "database_url": settings.DATABASE_URL,
+        "jwt_algorithm": settings.ALGORITHM,
+        "access_token_expire_minutes": settings.ACCESS_TOKEN_EXPIRE_MINUTES,
+        "refresh_token_expire_days": settings.REFRESH_TOKEN_EXPIRE_DAYS,
+        "secret_key_set": settings.SECRET_KEY
+    }
